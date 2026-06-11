4f842fad24239de80dbd190fa082089838944b80
Miyuki Nakani <miyukinakani89@gmail.com>
2026-06-11 00:54:58 -0400

    Actualizacion 2.0

--- Program.cs
+++ Program.cs
@@ -26,200 +26,103 @@ class Program
         List<Animal> zoologico = new List<Animal>()
         {
             // AVES
-            new Ave("Águila Real", 5, "Ave rapaz", true),
-            new Ave("Pingüino Emperador", 8, "Ave marina", false),
+            new Ave("Águila Real", 5, "Ave rapaz", true,"sonido de ave", true),
+            new Ave("Pingüino Emperador", 8, "Ave marina", false, "sonido de ave", false),
 
             // MAMÍFEROS
-            new Mamifero("León", 7, "Felino", "Sabana"),
-            new Mamifero("Delfín", 12, "Mamífero marino", "Océano"),
+            new Mamifero("León", 7, "Felino", "Sabana", "Gruñido", "Carnívoro"),
+            new Mamifero("Delfín", 12, "Mamífero marino", "Mar","Sonido de delphin", "Pescado"),
 
             // ANFIBIOS
             
-new Anfibio(
-    "Rana Roja",
-    2,
-    "Rana venenosa",
-    true,
-    "Selva tropical",
-    "Croac croac"
-),
+new Anfibio(  "Rana Roja",2, "Rana venenosa", true,"Selva tropical","Croac croac", "Insectos"),
 
-new Anfibio(
-    "Sapo Común",
-    4,
-    "Sapo",
-    false,
-    "Bosques y jardines",
-    "Croooac"
-),
+new Anfibio("Sapo Común",4,"Sapo",false,"Bosques y jardines","Croooac", "Lombrices"),
 
-new Anfibio(
-    "Ajolote",
-    3,
-    "Salamandra acuática",
-    false,
-    "Lagos y canales",
-    "Sonido suave"
-),
+new Anfibio(   "Ajolote",3,"Salamandra acuática",false,"Lagos y canales","Sonido suave", "Camarones"),
 
-new Anfibio(
-    "Salamandra Tigre",
-    5,
-    "Salamandra",
-    false,
-    "Bosques húmedos",
-    "Chirrido"
-),
+new Anfibio("Salamandra Tigre",5,"Salamandra", false, "Bosques húmedos", "Chirrido", "Insectos"),
 
-new Anfibio(
-    "Rana Toro",
-    4,
-    "Rana",
-    false,
-    "Pantanos",
-    "BRROOOOM"
-),
+new Anfibio("Rana Toro",4,"Rana",false,"Pantanos","BRROOOOM", "Insectos"),
 
-new Anfibio(
-    "Rana Arborícola",
-    2,
-    "Rana",
-    false,
-    "Árboles de zonas húmedas",
-    "Cri cri"
-),
+new Anfibio("Rana Arborícola",2,"Rana",false,"Árboles de zonas húmedas","Cri cri", "Insectos"),
 
-new Anfibio(
-    "Tritón Alpino",
-    3,
-    "Tritón",
-    false,
-    "Lagunas de montaña",
-    "Chii"
-),
+new Anfibio( "Tritón Alpino", 3, "Tritón", false, "Lagunas de montaña", "Chii", "Insectos"),
 
-new Anfibio(
-    "Rana Dardo Azul",
-    1,
-    "Rana venenosa",
-    true,
-    "Selvas tropicales",
-    "Croac"
-),
+new Anfibio("Rana Dardo Azul", 1,"Rana venenosa",true,"Selvas tropicales","Croac", "Insectos"),
 
-new Anfibio(
-    "Salamandra Gigante",
-    10,
-    "Salamandra",
-    false,
-    "Ríos de montaña",
-    "Gruñido suave"
-),
+new Anfibio("Salamandra Gigante",10,"Salamandra",false,"Ríos de montaña","Gruñido suave ", "Insectos"),
 
-new Anfibio(
-    "Rana de Cristal",
-    2,
-    "Rana",
-    false,
-    "Bosques tropicales",
-    "Croac transparente"
-)
+new Anfibio( "Rana de Cristal", 2, "Rana", false, "Bosques tropicales", "Croac transparente", "Insectos")
         };
 
-        foreach (Animal animal in zoologico)
+        bool continuar = true;
+        while (continuar)
         {
-            PresentarAnimal(animal);
-        }
+            Console.WriteLine("\n╔══════════════════════════════════════════════════╗");
+            Console.WriteLine("║                  Menú Principal                  ║");
+            Console.WriteLine("╚══════════════════════════════════════════════════╝");
+            Console.WriteLine("1. Ver todos los animales");
+            Console.WriteLine("2. Buscar un animal");
+            Console.WriteLine("3. Registrar nuevo animal");
+            Console.WriteLine("4. Modificar un animal");
+            Console.WriteLine("5. Eliminar un animal");
+            Console.WriteLine("6. Cantidad de animales");
+            Console.WriteLine("7. Salir");
+            Console.WriteLine("═════════════════════════");
+            Console.Write("Seleccione una opción: ");
 
-        // Funciones agregadas para modificar y eliminar registros.
-        ModificarAnimal(zoologico);
+            string? opcion = Console.ReadLine();
 
-        EliminarAnimal(zoologico);
-
-        Console.WriteLine("\nLista actualizada:\n");
-
-        foreach (Animal animal in zoologico)
-        {
-            PresentarAnimal(animal);
-        }
-
-        Console.WriteLine("Fin del recorrido por el zoológico.");
-    }
-
-    static void PresentarAnimal(Animal animal)
-    {
-        Console.WriteLine("---------------------------------");
-
-        animal.MostrarInformacion();
-
-        Console.WriteLine($"Sonido: {animal.HacerSonido()}");
-
-        Console.WriteLine("---------------------------------\n");
-
-    }
-   
-    // Busca un animal y actualiza sus datos.
-
-    static void ModificarAnimal(List<Animal> zoologico)
-    {
-        Console.Write("Ingrese el nombre del animal a modificar: ");
-        string nombreBuscado = Console.ReadLine();
-
-        foreach (Animal animal in zoologico)
-        {
-            if (animal.Nombre == nombreBuscado)
+            switch (opcion)
             {
-                Console.Write("Nuevo nombre: ");
-                string nuevoNombre = Console.ReadLine();
+                case "1":
+                    Console.WriteLine("\n Lista de todoss los animales\n");
+                    MantenimientoZoo.MostrarInformacion(zoologico);
+                    Console.WriteLine("\nPresione Enter para continuar...");
+                    Console.ReadLine();
+                    break;
 
-                Console.Write("Nueva edad: ");
-                int nuevaEdad = int.Parse(Console.ReadLine());
+                case "2":
+                    MantenimientoZoo.BuscarAnimal(zoologico);
+                    break;
 
-                Console.Write("Nueva especie: ");
-                string nuevaEspecie = Console.ReadLine();
+                case "3":
+                    MantenimientoZoo.RegistrarAnimal(zoologico);
+                    break;
 
-                animal.ModificarDatos(
-                    nuevoNombre,
-                    nuevaEdad,
-                    nuevaEspecie
-                );
+                case "4":
+                    MantenimientoZoo.ModificarAnimal(zoologico);
+                    Console.WriteLine("\nPresione Enter para continuar...");
+                    Console.ReadLine();
+                    break;
 
-                Console.WriteLine("Datos actualizados correctamente.");
-                return;
+                case "5":
+                    MantenimientoZoo.EliminarAnimal(zoologico);
+                    Console.WriteLine("\nPresione Enter para continuar...");
+                    Console.ReadLine();
+                    break;
+
+                case "6":
+                    MantenimientoZoo.CantidadAnimales(zoologico);
+                    break;
+
+                case "7":
+                    continuar = false;
+                    Console.WriteLine("¡Gracias por su visita!");
+                    Console.WriteLine("Saliendo...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida. ");
+                    Console.WriteLine("\nPresione Enter para continuar...");
+                    Console.ReadLine();
+                    break;
             }
         }
-
-        Console.WriteLine("No se encontró el animal.");
     }
-    // Busca un animal por nombre y lo elimina de la lista.
-    static void EliminarAnimal(List<Animal> zoologico)
-    {
-        Console.Write("Ingrese el nombre del animal a eliminar: ");
-        string nombreBuscado = Console.ReadLine();
 
-        Animal animalEliminar = null;
 
-        foreach (Animal animal in zoologico)
-        {
-            if (animal.Nombre == nombreBuscado)
-            {
-                animalEliminar = animal;
-                break;
-            }
-        }
-
-        if (animalEliminar != null)
-        {
-            zoologico.Remove(animalEliminar);
-
-            Console.WriteLine("Animal eliminado correctamente.");
-        }
-        else
-        {
-            Console.WriteLine("No se encontró el animal.");
-        }
-    }
-    //Registrar Datos
-   
 
 }
+
